8c2368fad56a2efbfabebab17da652fcf9283b71
wuhao150313 <1840165925@qq.com>
2025-04-21 15:58:21 +0800

    第一版

---
+++ src/test/java/top/wuhao/javaservice/JavaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package top.wuhao.javaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
